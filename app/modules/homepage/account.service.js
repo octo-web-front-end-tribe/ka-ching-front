c34f3f138efb087effda00feb8a27494093ddf73
François Petitit <fpetitit@octo.com>
2015-09-03 14:01:36 +0200

    get account balance using API

---
+++ app/modules/homepage/account.service.js
@@ -0,0 +1,19 @@
+(function () {
+    var app = angular.module("kaching.homepage");
+    app.factory("AccountService", ["$http", accountService]);
+
+    function accountService($http) {
+        return {
+            getAccount: function (callback) {
+                $http({url: 'http://localhost:3000/api/account', method: 'GET'})
+                    .success(function (data) {
+                        callback(null, data);
+                    })
+                    .error(function () {
+                        callback('error');
+                    });
+            }
+        }
+    }
+})
+();
--- app/modules/homepage/homepage.ctrl.js
+++ app/modules/homepage/homepage.ctrl.js
@@ -1,11 +1,10 @@
 (function () {
     var app = angular.module("kaching.homepage");
-    app.controller("HomepageController", ["$location", "FriendsService", homepageController]);
+    app.controller("HomepageController", ["$location", "FriendsService", "AccountService", homepageController]);
 
-    function homepageController($location, FriendsService) {
+    function homepageController($location, FriendsService, AccountService) {
 
         var vm = this;
-        vm.balance = 123;
 
         FriendsService.getFriends(function (error, data) {
             if (error) {
@@ -15,5 +14,15 @@
                 vm.friends = data;
             }
         });
+
+        AccountService.getAccount(function(error, data){
+            if(error) {
+                console.error('erreur lors de la récupération des comptes');
+                $location.url('/login');
+            } else {
+                vm.balance = data.balance;
+            }
+        });
+
     }
 })();
