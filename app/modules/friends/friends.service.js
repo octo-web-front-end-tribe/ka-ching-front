c838b1f41950036ddca09db4e042d218470b7cc4
Michael Akbaraly <makbaraly@octo.com>
2015-09-16 18:56:03 +0200

    - get friends from the api

--- app/modules/friends/friends.service.js
+++ app/modules/friends/friends.service.js
@@ -5,10 +5,7 @@
   function friendsService($http) {
     return {
       getFriends: function () {
-        return $http({url: "http://localhost:3000/api/friends", method: "GET"});
-      },
-      addFriend: function () {
-        return $http({url: "http://localhost:3000/api/friends", method: "POST"});
+        return $http({url: "http://localhost:3000/api/friends/doe", method: "GET"});
       }
     };
 
